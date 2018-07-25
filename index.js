142f52c0509ed09927900bf0f407446dc2c91ad0
ChenJing <502269499@qq.com>
2018-07-25 15:26:09 +0800

    跨域配置需在路由之前

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ const Koa = require('koa')
 const app = new Koa()
 const bodyParser = require('koa-bodyparser')
 app.use(bodyParser()) // 在路由之前
-const router = require('./router')
-app.use(require('koa-static')(path.join(__dirname, 'static'), {}))
-
-app.use(router.routes()).use(router.allowedMethods())
 const cors = require('koa2-cors')
+app.use(require('koa-static')(path.join(__dirname, 'static'), {}))
 
 // 跨域授权
 app.use(cors({
@@ -20,6 +17,9 @@ app.use(cors({
   allowMethods: ['GET', 'POST', 'OPTIONS'],
   allowHeaders: ['Keep-Alive', 'User-Agent', 'Content-Type', 'Authorization']
 }))
+
+const router = require('./router')
+app.use(router.routes()).use(router.allowedMethods())
 
 app.listen(4500)
 console.log('listening on 4500 port')
